e4b8b7381574105518bb878950ced18e78eb0e15
Alex Lo Storto <lostorto.business@gmail.com>
2023-02-04 19:22:41 +0000

    Updated Drawing_Grid.py

--- Drawing_Grid.py
+++ Drawing_Grid.py
@@ -2,37 +2,45 @@
 import pandas as pd
 
 
+def isFloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
 def getInput(prompt):
     userInput = input(prompt)
-    if userInput.isnumeric():
-        return int(userInput)
+    if isFloat(userInput):
+        return userInput
     else:
         getInput(prompt)
 
 
 def main():
-    rows = getInput("Number of rows: ")
-    columns = getInput("Number of columns: ")
-    height = getInput("Grid height: ")
-    width = getInput("Grid width: ")
+    rows = int(getInput("Number of rows: "))
+    columns = int(getInput("Number of columns: "))
+    height = float(getInput("Grid height: "))
+    width = float(getInput("Grid width: "))
 
     cellHeight = height/rows
     cellWidth = width/columns
 
     print("\n\n-----GRID DIMENSIONS-----")
-    print(f"Cell Height: {cellHeight}")
-    print(f"Cell Width: {cellWidth}\n\n")
+    print(f"Cell Height: {round(cellHeight, 1)}")
+    print(f"Cell Width: {round(cellWidth, 1)}\n\n")
 
     grid = []
 
     column = []
-    for i in range(rows + 1):
-        column.append(i * cellHeight)
+    for i in range(columns + 1):
+        column.append(round(i * cellWidth, 1))
     grid.append(column)
 
-    for i in range(columns + 1):
-        column = [i * cellWidth]
-        for i in range(rows):
+    for i in range(rows):
+        column = [round((i + 1) * cellHeight, 1)]
+        for i in range(columns):
             column.append("_")
         grid.append(column)
 
